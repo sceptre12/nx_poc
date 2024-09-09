2466a1fece0392ed8d0479a19c21ec70c871842a
Xavier Thomas <x.devr.space@gmail.com>
2024-09-09 01:37:14 -0400

    Added in the Zephyr Integration for the rsbuild project

--- packages/rsbuild_project/rsbuild.config.mjs
+++ packages/rsbuild_project/rsbuild.config.mjs
@@ -1,6 +1,13 @@
 import { defineConfig } from '@rsbuild/core';
+import { withZephyr } from 'zephyr-webpack-plugin';
 import { pluginReact } from '@rsbuild/plugin-react';
 
 export default defineConfig({
   plugins: [pluginReact()],
+  tools: {
+    rspack: (config) => {
+      // Adds in the zephyr configuration
+      return withZephyr()(config);
+    },
+  },
 });
