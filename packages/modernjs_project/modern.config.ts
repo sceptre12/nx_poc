477d770e809ffd6a2a53aafba1cd64e6cf249e7e
Xavier Thomas <x.devr.space@gmail.com>
2024-09-18 20:51:41 -0400

    Updating the modernjs project config so that it can launch all of the remotes it needs

--- packages/modernjs_project/modern.config.ts
+++ packages/modernjs_project/modern.config.ts
@@ -1,6 +1,6 @@
 import { appTools, defineConfig } from '@modern-js/app-tools';
 import { moduleFederationPlugin } from '@module-federation/modern-js';
-import {MODERNJS_PROJECT_PORT} from '../../libs/global-constants/src/index.cjs';
+import { MODERNJS_PROJECT_PORT } from '../../libs/global-constants/src/index.cjs';
 
 // NOTE Zephyr is not compatible with Modern right now but its in the works
 
@@ -13,12 +13,12 @@ export default defineConfig<'rspack'>({
     port: MODERNJS_PROJECT_PORT,
   },
   source: {
-    enableAsyncEntry: true
+    enableAsyncEntry: true,
   },
   plugins: [
     appTools({
       bundler: 'rspack', // Set to 'webpack' to enable webpack
     }),
-    moduleFederationPlugin()
+    moduleFederationPlugin(),
   ],
 });
